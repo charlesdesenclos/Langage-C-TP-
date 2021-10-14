c7c784bb382638e1e55845984bcc8b4e464595ee
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2021-10-14 16:07:31 +0200

    Update Exercice_5.cpp

--- TP3/Exercice_5/Exercice_5.cpp
+++ TP3/Exercice_5/Exercice_5.cpp
@@ -20,13 +20,13 @@ int main()
 		//on va faire une boucle if pur si str possède un espace alors on enlevera un espace 
 		if ((str[i] == ' ' && str[i+0] == ' ') !=1) // i+0 permet d'enlever l'espace
 		{ 
-			res[j] = str[i];
+			res[j] = str[i]; //on stocke str dans res pour avoir les modifs
 			j++;
 		}
 		i++;
 	}
 	res[j] = '\0';
-	printf("Voici la chaine de caractere sans les espace : %s", res);
+	printf("Voici la chaine de caractere sans les espace : %s", res); 
 	return 0;
 }
 
